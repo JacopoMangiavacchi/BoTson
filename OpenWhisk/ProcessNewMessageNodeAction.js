7907389a85df17c55bdea2c563bdff5476e46d62
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2016-10-12 18:08:43 +0100

    Added OpenWhisk actions

---
+++ OpenWhisk/ProcessNewMessageNodeAction.js
@@ -0,0 +1,19 @@
+/**
+  *
+  * main() will be invoked when you Run This Action.
+  * 
+  * @param Whisk actions accept a single parameter,
+  *        which must be a JSON object.
+  *
+  * In this case, the params variable will look like:
+  *     { "message": "xxxx" }
+  *
+  * @return which must be a JSON object.
+  *         It will be the output of this action.
+  *
+  */
+function main(params) {
+	var response = params;
+	response.messageText = "ECHO from OpenWhisk: " + params.messageText;
+	return response;
+}
---
+++ OpenWhisk/SendBackResponseToKituraBot.js
@@ -0,0 +1,59 @@
+/**
+  *
+  * main() will be invoked when you Run This Action.
+  * 
+  * @param Whisk actions accept a single parameter,
+  *        which must be a JSON object.
+  *
+  * In this case, the params variable will look like:
+  *     { "message": "xxxx" }
+  *
+  * @return which must be a JSON object.
+  *         It will be the output of this action.
+  *
+  */
+
+/// IN JSON Payload
+/// {
+///     "botUrl" : "https://...",
+///     "channel" : "xxx",
+///     "userId" : "xxx",
+///     "messageText" : "xxx",
+///     "securityToken" : "xxx"
+///     "context" : {}
+/// }
+
+
+/// OUT JSON Payload
+/// {
+///     "channel" : "xxx",
+///     "recipientId" : "xxx",
+///     "messageText" : "xxx",
+///     "securityToken" : "xxx"
+///     "context" : {}
+/// }
+
+var request = require('request');
+
+function main(params) {
+    var url = params.botUrl;
+
+	var payload = { channel: params.channel, recipientId: params.userId, messageText: params.messageText, securityToken: params.securityToken, context: params.context };
+
+    return new Promise(function(resolve, reject) {
+		var options = {
+		  uri: url,
+		  method: 'POST',
+		  json: payload
+		};
+
+		request(options, function(error, response, body) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve({msg: "Message Sent"});
+            }
+		});
+    });
+}
